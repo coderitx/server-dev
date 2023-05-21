5307815cb652f2dbe767c2f19bf528ad8644bf63
scliang <354767929@qq.com>
2023-05-21 19:11:42 +0800

    完善图片管理信息

---
+++ blog-server/apps/api/images_api/image_name_list.go
@@ -0,0 +1,35 @@
+package images_api
+
+import (
+	"blog-server/apps/models"
+	"blog-server/common/errorx"
+	"blog-server/common/responsex"
+	"blog-server/global"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+type ImageNameListResponse struct {
+	ID   uint   `json:"id"`
+	Path string `json:"path"`
+	Name string `json:"name"`
+}
+
+// ImageNameListView 查询图片名称信息列表
+// @Tags 图片管理
+// @Summary 图片名称信息列表
+// @Description 图片名称信息列表
+// @Router /api/imageNameList [get]
+// @Produce json
+// @Success 200 {object} []ImageNameListResponse{}
+func (*ImagesApi) ImageNameListView(c *gin.Context) {
+	var images []ImageNameListResponse
+	err := global.DB.Model(&models.BannerModel{}).Select("id", "name", "path").Scan(&images).Error
+	if err != nil {
+		zap.S().Error("query image list error: %v", err.Error())
+		responsex.FailWithCode(errorx.SettingsError, c)
+		return
+	}
+	responsex.OkWithData(images, c)
+	return
+}
